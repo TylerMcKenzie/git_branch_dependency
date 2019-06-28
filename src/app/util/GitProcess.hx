b918a10411acd10b30e69d9963be6cb7a178eacc
tylermckenzie <tylrmckenz@gmail.com>
2019-06-28 15:39:15 +0000

    inline functions for performance gains

--- src/app/util/GitProcess.hx
+++ src/app/util/GitProcess.hx
@@ -3,52 +3,52 @@ package app.util;
 import sys.io.Process;
 
 class GitProcess {
-    public static function branch(args: Array<String>) : Process
+    static public inline function branch(args: Array<String>) : Process
     {
         return new Process("git", ["branch"].concat(args));
     }
 
-    public static function commit(args: Array<String>) : Process
+    static public inline function commit(args: Array<String>) : Process
     {
         return new Process("git", ["commit"].concat(args));
     }
 
-    public static function config(args: Array<String>) : Process
+    static public inline function config(args: Array<String>) : Process
     {
         return new Process("git", ["config"].concat(args));
     }
 
-    public static function diff(args: Array<String>) : Process
+    static public inline function diff(args: Array<String>) : Process
     {
         return new Process("git", ["diff"].concat(args));
     }
 
-    public static function fetch(args: Array<String>) : Process
+    static public inline function fetch(args: Array<String>) : Process
     {
         return new Process("git", ["fetch"].concat(args));
     }
 
-    public static function pull(args: Array<String>) : Process
+    static public inline function pull(args: Array<String>) : Process
     {
         return new Process("git", ["pull"].concat(args));
     }
 
-    public static function remote(args: Array<String>) : Process
+    static public inline function remote(args: Array<String>) : Process
     {
         return new Process("git", ["remote"].concat(args));
     }
 
-    public static function reset(args: Array<String>) : Process
+    static public inline function reset(args: Array<String>) : Process
     {
         return new Process("git", ["reset"].concat(args));
     }
 
-    public static function revList(args: Array<String>) : Process
+    static public inline function revList(args: Array<String>) : Process
     {
         return new Process("git", ["rev-list"].concat(args));
     }
 
-    public static function revParse(args: Array<String>) : Process
+    static public inline function revParse(args: Array<String>) : Process
     {
         return new Process("git", ["rev-parse"].concat(args));
     }
