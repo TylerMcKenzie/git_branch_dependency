8e46bb4eb7fcd7119a81e506ba4f23c39f57ca65
tylermckenzie <tylrmckenz@gmail.com>
2019-06-28 14:48:23 +0000

    command returns int not bool

--- src/app/util/GitCommand.hx
+++ src/app/util/GitCommand.hx
@@ -1,52 +1,52 @@
 package app.util;
 
 class GitCommand {
-    public static function branch(args: Array<String>) : Bool
+    public static function branch(args: Array<String>) : Int
     {
         return Sys.command("git branch", args);
     }
 
-    public static function commit(args: Array<String>) : Bool
+    public static function commit(args: Array<String>) : Int
     {
         return Sys.command("git commit", args);
     }
 
-    public static function config(args: Array<String>) : Bool
+    public static function config(args: Array<String>) : Int
     {
         return Sys.command("git config", args);
     }
 
-    public static function diff(args: Array<String>) : Bool
+    public static function diff(args: Array<String>) : Int
     {
         return Sys.command("git diff", args);
     }
 
-    public static function fetch(args: Array<String>) : Bool
+    public static function fetch(args: Array<String>) : Int
     {
         return Sys.command("git fetch", args);
     }
 
-    public static function pull(args: Array<String>) : Bool
+    public static function pull(args: Array<String>) : Int
     {
         return Sys.command("git pull", args);
     }
 
-    public static function remote(args: Array<String>) : Bool
+    public static function remote(args: Array<String>) : Int
     {
         return Sys.command("git remote", args);
     }
 
-    public static function reset(args: Array<String>) : Bool
+    public static function reset(args: Array<String>) : Int
     {
         return Sys.command("git reset", args);
     }
 
-    public static function revList(args: Array<String>) : Bool
+    public static function revList(args: Array<String>) : Int
     {
         return Sys.command("git rev-list", args);
     }
 
-    public static function revParse(args: Array<String>) : Bool
+    public static function revParse(args: Array<String>) : Int
     {
         return Sys.command("git rev-parse", args);
     }
