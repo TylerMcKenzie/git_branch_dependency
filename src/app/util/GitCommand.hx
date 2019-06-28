baba0cc4b68e26b63b541a258b9c3fc361b07e97
tylermckenzie <tylrmckenz@gmail.com>
2019-06-28 15:33:43 +0000

    update to concat args to process calls

--- src/app/util/GitCommand.hx
+++ src/app/util/GitCommand.hx
@@ -3,51 +3,51 @@ package app.util;
 class GitCommand {
     public static function branch(args: Array<String>) : Int
     {
-        return Sys.command("git branch", args);
+        return Sys.command("git", ["branch"].concat(args));
     }
 
     public static function commit(args: Array<String>) : Int
     {
-        return Sys.command("git commit", args);
+        return Sys.command("git", ["commit"].concat(args));
     }
 
     public static function config(args: Array<String>) : Int
     {
-        return Sys.command("git config", args);
+        return Sys.command("git", ["config"].concat(args));
     }
 
     public static function diff(args: Array<String>) : Int
     {
-        return Sys.command("git diff", args);
+        return Sys.command("git", ["diff"].concat(args));
     }
 
     public static function fetch(args: Array<String>) : Int
     {
-        return Sys.command("git fetch", args);
+        return Sys.command("git", ["fetch"].concat(args));
     }
 
     public static function pull(args: Array<String>) : Int
     {
-        return Sys.command("git pull", args);
+        return Sys.command("git", ["pull"].concat(args));
     }
 
     public static function remote(args: Array<String>) : Int
     {
-        return Sys.command("git remote", args);
+        return Sys.command("git", ["remote"].concat(args));
     }
 
     public static function reset(args: Array<String>) : Int
     {
-        return Sys.command("git reset", args);
+        return Sys.command("git", ["reset"].concat(args));
     }
 
     public static function revList(args: Array<String>) : Int
     {
-        return Sys.command("git rev-list", args);
+        return Sys.command("git", ["rev-list"].concat(args));
     }
 
     public static function revParse(args: Array<String>) : Int
     {
-        return Sys.command("git rev-parse", args);
+        return Sys.command("git", ["rev-parse"].concat(args));
     }
 }
--- src/app/util/GitProcess.hx
+++ src/app/util/GitProcess.hx
@@ -5,51 +5,51 @@ import sys.io.Process;
 class GitProcess {
     public static function branch(args: Array<String>) : Process
     {
-        return new Process("git branch", args);
+        return new Process("git", ["branch"].concat(args));
     }
 
     public static function commit(args: Array<String>) : Process
     {
-        return new Process("git commit", args);
+        return new Process("git", ["commit"].concat(args));
     }
 
     public static function config(args: Array<String>) : Process
     {
-        return new Process("git config", args);
+        return new Process("git", ["config"].concat(args));
     }
 
     public static function diff(args: Array<String>) : Process
     {
-        return new Process("git diff", args);
+        return new Process("git", ["diff"].concat(args));
     }
 
     public static function fetch(args: Array<String>) : Process
     {
-        return new Process("git fetch", args);
+        return new Process("git", ["fetch"].concat(args));
     }
 
     public static function pull(args: Array<String>) : Process
     {
-        return new Process("git pull", args);
+        return new Process("git", ["pull"].concat(args));
     }
 
     public static function remote(args: Array<String>) : Process
     {
-        return new Process("git remote", args);
+        return new Process("git", ["remote"].concat(args));
     }
 
     public static function reset(args: Array<String>) : Process
     {
-        return new Process("git reset", args);
+        return new Process("git", ["reset"].concat(args));
     }
 
     public static function revList(args: Array<String>) : Process
     {
-        return new Process("git rev-list", args);
+        return new Process("git", ["rev-list"].concat(args));
     }
 
     public static function revParse(args: Array<String>) : Process
     {
-        return new Process("git rev-parse", args);
+        return new Process("git", ["rev-parse"].concat(args));
     }
 }
